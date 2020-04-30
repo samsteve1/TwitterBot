8c689166c0b1be74b43b93ece52c1dda7341c9f1
Steve Idowu <idowusteve@ymail.com>
2020-04-30 21:11:23 +0100

    Adding emojis to tweet

--- app/Console/Commands/ReplyToTweets.php
+++ app/Console/Commands/ReplyToTweets.php
@@ -2,9 +2,12 @@
 
 namespace App\Console\Commands;
 
+use App\Common\EmojiHelper;
+use App\Jobs\SendTweet;
 use App\Services\Twitter\Exceptions\RateLimitExceededException;
 use App\Services\Twitter\TwitterService;
 use App\Tracking;
+use Exception;
 use Illuminate\Console\Command;
 
 class ReplyToTweets extends Command
@@ -30,7 +33,7 @@ class ReplyToTweets extends Command
      *
      * @return void
      */
-    public function __construct(TwitterService $twitter)
+    public function __construct(TwitterService $twitter, EmojiHelper $emojis)
     {
         parent::__construct();
         $this->twitter = $twitter;
@@ -46,10 +49,9 @@ class ReplyToTweets extends Command
     {
         $tracked = $tracking->latestFirst();
 
-        try{
+        try {
             $mentions = $this->twitter->getMentions($tracked->count() ? $tracked->first()->twitter_id : null);
-            
-        } catch(RateLimitExceededException $e) {
+        } catch (Exception $e) {
             return $this->error($e->getMessage());
         }
 
@@ -57,12 +59,18 @@ class ReplyToTweets extends Command
             return $this->info('No mentions to process at the moment');
         }
 
-        $text = $mentions->map(function($mention) {
+        $text = $mentions->map(function ($mention) {
             return $mention->text;
         });
 
         $sentiments = $this->ml->classifiers->classify('cl_pi3C7JiL', $text->toArray(), true);
 
-        dd($sentiments);
+        $mentions->each(function ($mention, $index) use ($sentiments, $emojis) {
+            dispatch(new SendTweet(
+                $mention->id,
+                $mention->user->screen_name,
+                $emojis->random($sentiments->result[$index][0]['label'])
+            ));
+        });
     }
 }
--- app/Services/Twitter/CodeBirdTwitterService.php
+++ app/Services/Twitter/CodeBirdTwitterService.php
@@ -5,6 +5,7 @@ namespace App\Services\Twitter;
 use App\Services\Twitter\Exceptions\RateLimitExceededException;
 use App\Services\Twitter\TwitterService;
 use Codebird\Codebird;
+use Exception;
 
 class CodeBirdTwitterService implements TwitterService
 {
@@ -16,17 +17,30 @@ class CodeBirdTwitterService implements TwitterService
     }
     public function getMentions($since = null)
     {
-        $mentions = $this->client->statuses_mentionsTimeline($since ? 'since_id='. $since: '');
+        try{
+            $mentions = $this->client->statuses_mentionsTimeline($since ? 'since_id='. $since: '');
 
         if ((int) $mentions->rate->remaining === 0) {
             throw new RateLimitExceededException;
+            exit(1);
         }
 
         return collect($this->extractTweets($mentions));
+        } catch(Exception $e){
+            throw new Exception('Unable to fetch mentions.');
+        }
     }
     public function sendTweet($tweet, $inReplyTo = null)
     {
-        return 'send tweet';
+        $params = [
+            'status' => $tweet,
+        ];
+
+        if ($inReplyTo) {
+            $params['in_reply_to_status'] = $inReplyTo;
+        }
+
+        $this->client->statuses_update($params);
     }
 
     protected function extractTweets($response)
